a07fe2156b510e799f81c8abad768de7f74710ae
lizejia <376489098@qq.com>
2016-11-04 15:18:59 +0800

    测试代码
    测试代码

---
+++ UrlTest/UrlTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UrlTest.Global" Language="C#" %>
---
+++ UrlTest/UrlTest/Global.asax.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using UrlTest;
+
+namespace UrlTest
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // 在应用程序启动时运行的代码
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterOpenAuth();
+        }
+
+        void Application_End(object sender, EventArgs e)
+        {
+            //  Code that runs on application shutdown
+
+        }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            // Code that runs when an unhandled error occurs
+
+        }
+
+        void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapPageRoute(
+                "DefaultRoute",
+                "Default/{id}.html",
+                "~/Default.aspx");
+        } 
+    }
+}
